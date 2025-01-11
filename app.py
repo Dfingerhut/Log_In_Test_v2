752c549da5a61ee62c15ed1f5437c9762291bfa0
Dfingerhut <d.fingerhut38@gmail.com>
2025-01-11 13:32:20 -0700

    new file:   __pycache__/__init__.cpython-312.pyc 	new file:   __pycache__/app.cpython-312.pyc 	new file:   app.py 	new file:   requirements.txt 	new file:   templates/base.html 	new file:   templates/home.html 	new file:   templates/login.html 	new file:   templates/protected.html 	new file:   templates/signup.html

---
+++ app.py
@@ -0,0 +1,75 @@
+import os
+import functools
+from flask import (
+    Flask,
+    session,
+    render_template,
+    request,
+    abort,
+    flash,
+    redirect,
+    url_for,
+)
+from passlib.hash import pbkdf2_sha256
+
+app = Flask(__name__)
+# Secret key generated with secrets.token_urlsafe()
+app.secret_key = "lkaQT-kAb6aIvqWETVcCQ28F-j-rP_PSEaCDdTynkXA"
+
+users = {}
+
+
+def login_required(route):
+    @functools.wraps(route)
+    def route_wrapper(*args, **kwargs):
+        if session.get("email") is None:
+            return redirect(url_for("login"))
+
+        return route(*args, **kwargs)
+
+    return route_wrapper
+
+
+@app.get("/")
+def home():
+    return render_template("home.html", email=session.get("email"))
+
+
+@app.get("/protected")
+@login_required
+def protected():
+    return render_template("protected.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    email=''
+    
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if email in users.keys():
+            if pbkdf2_sha256.verify(password, users.get(email)):
+                session["email"] = email
+                return redirect(url_for("protected"))
+            else:
+                flash("Incorrect email or password!")
+    return render_template("login.html", email=email)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        users[email] = pbkdf2_sha256.hash(password)
+        flash("Successfully signed up.")
+        return redirect(url_for("login"))
+    return render_template("signup.html")
+
+
+@app.errorhandler(401)
+def auth_error(error):
+    return "Not authorized"
